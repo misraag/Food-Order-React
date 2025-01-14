66c340a72216bea9a648da4cf324eb002e8416fb
misraag <jaiofficial50@gmail.com>
2025-01-14 20:41:53 +0530

    added items-context and userProgressContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Cart from "./components/UI/Cart";
+import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Meals/>
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart/>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import { useContext } from 'react'
 import imgLogo from '../assets/logo.jpg'
 import Button from './UI/Button'
+import CartContext from '../store/CartContext'
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0)
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return <header id='main-header'>
         <div id='title'>
          <img src={imgLogo}></img>
          <h1>REACT FOODS</h1>
          </div>
-         <Button textOnly>Cart(0)</Button>
+         <Button textOnly onClick={handleShowCart}>Cart({totalCartItems})</Button>
     </header>
 }
